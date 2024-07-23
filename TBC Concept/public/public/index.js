4f0f0f5faf9421dad5533c881860f57617c1b389
KlasniMisho123 <117183990+KlasniMisho123@users.noreply.github.com>
2024-07-23 20:43:03 +0400

    Commit 8

---
+++ TBC Concept/public/public/index.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const flexLeanguageElement = document.querySelector(".flex-leanguage-element");
+    const langSectionElement = document.querySelector(".leanguage-section");
+
+    
+    const currentLeanguage = document.querySelector(".leanguage-text").textContent.trim();
+    console.log("current language: ",currentLeanguage)
+
+    const engContent = `
+      <form action="/en/" method="GET" class="language-form">       
+            <div class="leanguage-list-wrap">
+              <button class="language-btn">
+                <div class="leanguage-list">
+                    <h4 class="leanguage"> ENG </h4>
+                </div>
+              </button> 
+            </div>
+      </form>
+    `;
+
+    const geoContent = `
+      <form action="/ge/" method="GET" class="language-form">       
+            <div class="leanguage-list-wrap">
+              <button class="language-btn">
+                <div class="leanguage-list">
+                    <h4 class="leanguage"> ქარ </h4>
+                </div>
+              </button> 
+            </div>
+      </form>
+    `;
+
+    let contentAdded = false;
+
+    flexLeanguageElement.addEventListener("mouseover", function() {
+        if (!contentAdded) {
+            if(currentLeanguage === "Eng") {
+              langSectionElement.insertAdjacentHTML('beforeend', geoContent);
+              contentAdded = true;
+            } else {
+              langSectionElement.insertAdjacentHTML('beforeend', engContent);
+              contentAdded = true;
+            }
+        }
+    });
+
+    flexLeanguageElement.addEventListener("mouseleave", function() {
+        if (contentAdded) {
+            const addedContent = langSectionElement.querySelector(".language-form");
+            if (addedContent) {
+                addedContent.remove();
+                contentAdded = false;
+            }
+        }
+    });
+});
